c0d2145d21ca898c50372a983b0ce5cf9633d2bb
Abhijeet V.S <2308abhijeet@gmail.com>
2026-08-27 22:44:00 +0530

    Modify MONGO_URI for database connection
    Updated MONGO_URI to include database name and retry options.

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ from flask_cors import CORS
 from pymongo import MongoClient, DESCENDING
 from pymongo.errors import ConnectionFailure
 
-MONGO_URI    = os.getenv("MONGO_URI", "mongodb+srv://mineAdmin:Mine%40Safety2025@cluster0.sfxmvya.mongodb.net/?appName=Cluster0")
+MONGO_URI    = os.getenv("MONGO_URI", "mongodb+srv://mineAdmin:MineAdmin2025@cluster0.sfxmvya.mongodb.net/mineDB?retryWrites=true&w=majority&appName=Cluster0"")
 DB_NAME      = "mineDB"
 COLLECTION   = "helmetData"
 PORT         = int(os.getenv("PORT", 5000))
